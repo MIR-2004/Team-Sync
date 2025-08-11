8ad3fec7e4195969db7a7ec15ba81d45ce238d88
Mir Saif Ali <mir.saif.ali2004@gmail.com>
2025-08-11 17:56:23 +0530

    Created server

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import 'dotenv/config'
+import express, { NextFunction, Request, Response, urlencoded } from 'express'
+import cors from 'cors'
+import session from 'cookie-session'
+import { config } from './config/app.config';
+import connectDatabase from './config/database.config';
+import { errorHandler } from './middlewares/errorHandler.middlware';
+import { HTTPSTATUS } from './config/http.config';
+import { asyncHandler } from './middlewares/asyncHandler.middleware';
+
+const app = express();
+const BASE_PATH = config.BASE_PATH;
+
+
+app.use(express.json())
+app.use(urlencoded({ extended: true }))
+app.use(session({ name: "session", keys: [config.SESSION_SECRET], maxAge: 24 * 60 * 60 * 1000, secure: config.NODE_ENV === "production", httpOnly: true, sameSite: "lax" }))
+
+app.use(
+    cors({
+        origin: config.FRONTEND_ORIGIN,
+        credentials: true
+    })
+)
+
+
+app.get('/', asyncHandler((req: Request, res: Response, next: NextFunction) => {
+        throw new Error("Test error")
+        res.status(HTTPSTATUS.OK).json({
+            message: "Website is running"
+        });
+}))
+
+
+app.use(errorHandler);
+
+app.listen(config.PORT, async() => {
+    console.log(`Server is running on port ${config.PORT} in ${config.NODE_ENV}`);
+    await connectDatabase();
+})
+
+
+
